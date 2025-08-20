90613a18aedde812f375c6daf74f022daca26a77
Guanyu Lin <limkoaniun@gmail.com>
2025-08-20 23:30:03 +1000

    feat: improve vision effect of RenderUserMenu in Doctor.cs

--- HospitalManagementSystem/Doctor.cs
+++ HospitalManagementSystem/Doctor.cs
@@ -21,21 +21,26 @@ public class Doctor : User
         while (true)
         {
             Console.Clear();
-            Console.WriteLine("DOTNET Hospital Management System");
-            Console.WriteLine("---------------------------------");
-            Console.WriteLine("Doctor Menu\n");
-            Console.WriteLine($"Welcome to DOTNET Hospital Management System {FullName}\n");
-
-            Console.WriteLine("Please choose an option:");
-            Console.WriteLine("1. List doctor details");
-            Console.WriteLine("2. List patients");
-            Console.WriteLine("3. List appointments");
-            Console.WriteLine("4. Check particular patient");
-            Console.WriteLine("5. List appointments with patient");
-            Console.WriteLine("6. Logout");
-            Console.WriteLine("7. Exit");
-
-            Console.Write("\nEnter choice: ");
+            Console.WriteLine(" _____________________________________________________________");
+            Console.WriteLine(" |              DOTNET Hospital Management System            |");                             
+            Console.WriteLine(" |___________________________________________________________|");
+            Console.WriteLine(" |                         Doctor Menu                       |");
+            Console.WriteLine(" |___________________________________________________________|");
+            Console.WriteLine($"| Welcome to DOTNET Hospital Management System {FullName}   |");
+            Console.WriteLine(" |                                                           |");
+            Console.WriteLine(" | Please choose an option:                                  |");
+            Console.WriteLine(" |    1. List doctor details                                 |");
+            Console.WriteLine(" |    2. List patients                                       |");
+            Console.WriteLine(" |    3. List appointments                                   |");
+            Console.WriteLine(" |    4. Check particular patient                            |");
+            Console.WriteLine(" |    5. List appointments with patient                      |");
+            Console.WriteLine(" |    6. Logout                                              |");
+            Console.WriteLine(" |    7. Exit                                                |");
+            Console.WriteLine(" |___________________________________________________________|");
+            Console.WriteLine(" |                                                           |");
+            Console.WriteLine(" |    Enter your choice:                                     |");
+            Console.WriteLine(" |___________________________________________________________|");
+            Console.WriteLine();
             var choice = Console.ReadLine();
 
             switch (choice)
